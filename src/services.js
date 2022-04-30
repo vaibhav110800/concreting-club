6c762092658bf23c806333818451151919184d9b
vaibhav110800 <vibhu1108@gmail.com>
2022-05-01 01:24:38 +0530

    commit 6

--- src/services.js
+++ src/services.js
@@ -6,6 +6,46 @@ const Services = () => {
   return (
     <div>
       <Menubar />
+
+      <div className="os-1">
+        <div className="os-l">
+          <div className="os-l-h">
+            Our <br />Services
+          </div>
+        </div>
+        <div className="os-r">
+          <div className="os-r-p">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus eum dolorum, reprehenderit ratione consequatur aut rerum doloremque odit doloribus voluptate sint voluptatem iusto esse laudantium tenetur adipisci quisquam. Rem, nesciunt.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate dolor eum culpa minima ad praesentium inventore ratione nobis eligendi voluptatem, harum quis illo ex animi dicta! Quos non assumenda rerum!
+          </div>
+        </div>
+      </div>
+
+      <div className="os-2">
+        <h1>Concrete Paving</h1>
+        <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident libero voluptatibus exercitationem totam, facilis voluptatem nesciunt sequi vero, odit magnam ipsum, distinctio ab tempore molestias. Delectus inventore necessitatibus cupiditate voluptates!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut ad, temporibus, totam laboriosam accusantium unde alias quam est ab, deserunt blanditiis dolorem rerum quis tempore voluptas cupiditate dolor officia dignissimos.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis eaque asperiores, deleniti quasi atque et ipsam mollitia odio dolores sapiente eum incidunt assumenda? Veniam maxime maiores fuga esse veritatis id?
+        </h3>
+
+        <h1>Brick Paving</h1>
+        <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident libero voluptatibus exercitationem totam, facilis voluptatem nesciunt sequi vero, odit magnam ipsum, distinctio ab tempore molestias. Delectus inventore necessitatibus cupiditate voluptates!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis eaque asperiores, deleniti quasi atque et ipsam mollitia odio dolores sapiente eum incidunt assumenda? Veniam maxime maiores fuga esse veritatis id?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa aliquam sed unde voluptates, distinctio laudantium, accusamus deleniti suscipit eaque optio eius consectetur beatae fugiat placeat est, dolor praesentium blanditiis aperiam.
+        </h3>
+
+        <h1>Aggregrate Paving</h1>
+        <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident libero voluptatibus exercitationem totam, facilis voluptatem nesciunt sequi vero, odit magnam ipsum, distinctio ab tempore molestias. Delectus inventore necessitatibus cupiditate voluptates!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis eaque asperiores, deleniti quasi atque et ipsam mollitia odio dolores sapiente eum incidunt assumenda? Veniam maxime maiores fuga esse veritatis id?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis aperiam aliquam placeat necessitatibus. Officia, possimus. Porro error suscipit culpa. Magni pariatur quo eaque laboriosam. Laborum tempore minima ad odit corrupti.
+        </h3>
+
+        <h1>Colored Concrete Paving</h1>
+        <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident libero voluptatibus exercitationem totam, facilis voluptatem nesciunt sequi vero, odit magnam ipsum, distinctio ab tempore molestias. Delectus inventore necessitatibus cupiditate voluptates!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis eaque asperiores, deleniti quasi atque et ipsam mollitia odio dolores sapiente eum incidunt assumenda? Veniam maxime maiores fuga esse veritatis id?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita laborum odit blanditiis odio maiores reiciendis, possimus itaque nostrum illum! Tenetur, ratione vero porro rem ducimus voluptatibus accusantium distinctio aliquid libero?
+        </h3>
+      </div>
       <Footer />
     </div>
   );
